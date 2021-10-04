a8bb23928f05fd403828fcd973159037ee8a7151
Alex Friedman <signetcoding@gmail.com>
2021-10-04 08:29:33 +0800

    adds tests

--- src/_tests_/Home.test.tsx
+++ src/_tests_/Home.test.tsx
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Cat } from '../types/Cat';
+import { Breed } from '../types/Breed';
 import { Home } from '../routes/Home/Home';
 
 const initialState = {
   loading: true,
   selectedBreed: 'default',
   catList: [] as Cat[],
+  breedList: [] as Breed[],
   error: null,
   disappear: false,
 };
@@ -24,25 +27,9 @@ describe('Home component', () => {
     );
     const header = getByText(/Cat Browser/i);
     const buttonStatus = getByText(/Load more/i);
-    expect(header).toBeTruthy();
-    expect(buttonStatus).toBeTruthy();
-  });
-});
-
-describe('Home component not disabled', () => {
-  const setState = jest.fn();
-  const mockDispatch = jest.fn();
-
-  test('renders Cat Browser heading', () => {
-    const { getByText } = render(
-      <Home
-        setSelectedCat={setState}
-        state={initialState}
-        dispatch={mockDispatch}
-      />
-    );
-    const header = getByText(/Cat Browser/i);
-    const buttonStatus = getByText(/Loading cats.../i);
+    userEvent.click(getByText('Load more'));
+    
+    expect(getByText(/Load more/i)).toBeTruthy();
     expect(header).toBeTruthy();
     expect(buttonStatus).toBeTruthy();
   });
